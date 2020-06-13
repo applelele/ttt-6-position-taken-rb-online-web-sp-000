7bf8ab2069063f41195285ffc1dfb88bfd058ecf
YUKIKO IKEDA <applelele@gmail.com>
2020-06-13 20:22:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   int = index - 1
-  if board[int] == " " || board[int] == "" || board[int] == nill
+  if board[int] == " " || board[int] == "" || board[int] == nil
     false
   end
 end
