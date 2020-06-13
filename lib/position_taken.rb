6c0292a6dbf8c774045c92399df79687b213d699
YUKIKO IKEDA <applelele@gmail.com>
2020-06-13 20:26:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   int = index - 1
   if board[int] == " " || board[int] == "" || board[int] == nil
     false
-  else board[int] == "X" || board[int] == "O"
+  elsif board[int] == "X" || board[int] == "O"
     true
   end
 end
